ea5c0fe5fb9d9e75c7547fa67fc7ec36b96eac1c
ovinov <40320369+ovinov@users.noreply.github.com>
2021-01-15 01:34:00 +0300

    s
    s

--- js/delivery.js
+++ js/delivery.js
@@ -13,5 +13,15 @@ function quantity(){return data.reduce(function(a,b){
 },0)}
   document.getElementById("total").innerHTML =`Вы выбрали ${quantity()} товара на сумму ${cost()} `
 
+  export function sendEmail(){Email.send({
+     secureToken:"6c9f59d7-27fd-46a9-854b-88331c7f3667",
+      Host : "smtp.gmail.com",
+      Username : "fmpshop.mos@gmail.com",
+      Password : "Viva0000",
+      To : 'fmpshop.mos@gmail.com',
+      From : "fmpshop.mos@gmail.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  })}
 
 sendEmail()
