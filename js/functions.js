7f27fe6cfa3d1ca789d27de111395166dfb35db7
ovinov <40320369+ovinov@users.noreply.github.com>
2021-01-15 01:30:46 +0300

    1
    1

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+export function sendEmail(){Email.send({
+   secureToken:"6c9f59d7-27fd-46a9-854b-88331c7f3667",
+    Host : "smtp.gmail.com",
+    Username : "fmpshop.mos@gmail.com",
+    Password : "Viva0000",
+    To : 'fmpshop.mos@gmail.com',
+    From : "fmpshop.mos@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+})}
