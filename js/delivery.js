81468b0a4d3168d35fce2332d01d1ef88e8cb658
ovinov <40320369+ovinov@users.noreply.github.com>
2021-01-20 03:31:41 +0300

    s
    s

--- js/delivery.js
+++ js/delivery.js
@@ -11,7 +11,40 @@ function quantity(){return data.reduce(function(a,b){
   return a + b.count;
 },0)}
   document.getElementById("total").innerHTML =`Вы выбрали ${quantity()} товара на сумму ${cost()} `
+  var now = new Date();
+  function makeOrder(clother){
+  return  `
+  Продукт: ${clother.name}
+  Количество: ${clother.count}
+  Размер: ${clother.size}
+  Цена: ${clother.price}
+  Цвет: ${clother.color}
+  `  }
 
-
+  function contactInfo(){
+  var form = document.querySelectorAll('input')
+  return `
+  ФИО ${form[0].value}
+  ПОЧТА ${form[1].value}
+  ТЕЛЕФОН  ${form[2].value}
+  ГОРОД  ${form[3].value}
+  УЛИЦА  ${form[4].value}
+  ДОМ  ${form[5].value}
+  ПОДЪЕЗД ${form[6].value}
+  КВАРТИРА  ${form[7].value}
+  ИНДЕКС  ${form[8].value}
+    `
+  }
+  var order = data.map(makeOrder).join("")
+  console.log(order, cost())
 sendEmail()
-//s
+function sendEmail(){Email.send({
+    secureToken:"6c9f59d7-27fd-46a9-854b-88331c7f3667",
+     Host : "smtp.gmail.com",
+     Username : "fmpshop.mos@gmail.com",
+     Password : "Viva0000",
+     To : 'fmpshop.mos@gmail.com',
+     From : "fmpshop.mos@gmail.com",
+     Subject : "Новый заказ от " + now,
+     Body : order + cost() + contactInfo()
+ })}
